10a104e69a4214bf4ba8fec1f12f76093ed086c8
antoneville <antoneville@hotmail.com>
2018-09-24 17:07:52 +0100

    Abstract Files Setup

---
+++ ThreeDShape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace C_Lab_One_Abstract
+{
+    abstract class ThreeDShape
+    {
+        //Default to private - just using it for good practice
+        private Shape type;
+
+    }
+
+    class Shape {
+        private Cylinder c;
+        private Sphere s;
+        private Cone co;
+    }
+
+    class Cylinder {
+
+    }
+
+    class Sphere {
+        
+    }
+
+    class Cone {
+
+    }
+}
