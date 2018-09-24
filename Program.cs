429e91cc47d463abd0cc5f86a42d7330c8bd760f
antoneville <antoneville@hotmail.com>
2018-09-24 16:42:40 +0100

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Lab_One_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
